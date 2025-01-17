826ca763df4d2ac0bc6c8e262899d6daf71356db
georgia-lee04 <xgeorgialee@gmail.com>
2025-01-17 14:28:18 +0000

    Updated javascript to clean up select functionality

--- javascript/index.js
+++ javascript/index.js
@@ -38,7 +38,7 @@ function updateCity(event) {
     let citiesElement = document.querySelector("#cities")
     citiesElement.innerHTML = `<div class="city">
     <div>
-<h2>${cityTimeZone}</h2>
+<h2>${cityName}</h2>
 <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
 </div>
 <div class="time">${cityTime.format("hh:mm:ss")}<small>${cityTime.format("A")}</small></div>`
