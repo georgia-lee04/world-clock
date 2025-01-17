c796e405b8fff6774ccc5016fa70aebc2eedc0f4
georgia-lee04 <xgeorgialee@gmail.com>
2025-01-17 14:48:56 +0000

    Added link back to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -44,9 +44,10 @@ function updateCity(event) {
 <h2>${cityName}</h2>
 <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
 </div>
-<div class="time">${cityTime.format("hh:mm:ss")}<small>${cityTime.format("A")}</small></div>`
+<div class="time">${cityTime.format("hh:mm:ss")}<small>${cityTime.format("A")}</small></div>
 
-}
+<a href ="/">All cities</a>
+`}
 
 updateTime()
 setInterval(updateTime, 1000)
